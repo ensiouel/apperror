e46bb7fd97260ffdf4fcbc271d801c2a71c74c53
sima <ensiouel@mail.ru>
2023-05-30 19:55:19 +0300

    add more tests

--- apperror_test.go
+++ apperror_test.go
@@ -1,6 +1,7 @@
 package apperror_test
 
 import (
+	"errors"
 	"github.com/ensiouel/apperror"
 	"testing"
 )
@@ -14,5 +15,21 @@ func TestNew(t *testing.T) {
 
 	if err.Code != apperror.NotFound {
 		t.Errorf("expected %d, got %d", apperror.NotFound, err.Code)
+	}
+}
+
+func TestError_WithMessage(t *testing.T) {
+	err := apperror.New(apperror.NotFound).WithMessage("user not found")
+
+	if err.Message != "user not found" {
+		t.Errorf("expected %s, got %s", "user not found", err.Message)
+	}
+}
+
+func TestError_WithError(t *testing.T) {
+	err := apperror.New(apperror.Internal).WithError(errors.New("timeout"))
+
+	if err.Error() != "timeout" {
+		t.Errorf("expected %s, got %s", "timeout", err.Message)
 	}
 }
